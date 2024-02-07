87fff73efc8b657fb04381f27809622e3c555ba1
Nishant <nishant@warehousenow.in>
2024-02-07 11:41:44 +0530

    fixed undo functionality, and responsive design

--- pages/image-editor.jsx
+++ pages/image-editor.jsx
@@ -10,7 +10,6 @@ const ImageEditor = () => {
   const [actionObjects, setActionObjects] = useState([]);
   const [isDragging, setIsDragging] = useState(false);
   const [lastActiveTextId, setLastActiveTextId] = useState(0);
-  const [undoActions, setUndoActions] = useState([]);
 
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
@@ -132,11 +131,6 @@ const ImageEditor = () => {
 
   const undo = () => {
     if (lastActiveTextId > 0) {
-      setUndoActions((prevState) => [
-        ...prevState,
-        actionObjects[lastActiveTextId],
-      ]);
-
       const updatedTextObj = actionObjects.filter(
         (text) => text.id !== lastActiveTextId
       );
@@ -181,55 +175,81 @@ const ImageEditor = () => {
         style={{
           height: "80vh",
         }}
-        className="flex md:flex-row flex-col justify-between mt-6 px-10"
+        className="flex md:flex-row flex-col-reverse justify-between mt-6 md:px-10 px-2"
       >
         {/* Toolbar */}
         <div style={{ flex: 1, padding: "20px" }}>
           <div className="flex justify-between items-center gap-x-2">
-            <div className="flex justify-center items-center rounded-md ring-1 ring-gray-600 cursor-pointer">
-              <label htmlFor="file-input" className="cursor-pointer">
-                <div className="flex flex-row justify-center items-center gap-x-3 py-2 px-3">
-                  <span className="text-sm text-gray-600 pb-0.5">
-                    Upload Image
-                  </span>
+            <div className="flex gap-x-4">
+              <div className="flex justify-center items-center rounded-md ring-1 ring-gray-600 cursor-pointer">
+                <label htmlFor="file-input" className="cursor-pointer">
+                  <div className="flex flex-row justify-center items-center gap-x-3 py-2 px-3">
+                    <span className="text-sm text-gray-600 pb-0.5">
+                      Upload Image
+                    </span>
+                  </div>
+                </label>
+                <input
+                  id="file-input"
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageUpload}
+                  hidden
+                />
+              </div>
+              <button
+                type="button"
+                className="rounded-md cursor-pointer"
+                onClick={handleDownload}
+              >
+                <div className="flex flex-row justify-center items-center p-3">
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-7 h-7"
+                  >
+                    <path
+                      d="M17 9.00195C19.175 9.01406 20.3529 9.11051 21.1213 9.8789C22 10.7576 22 12.1718 22 15.0002V16.0002C22 18.8286 22 20.2429 21.1213 21.1215C20.2426 22.0002 18.8284 22.0002 16 22.0002H8C5.17157 22.0002 3.75736 22.0002 2.87868 21.1215C2 20.2429 2 18.8286 2 16.0002L2 15.0002C2 12.1718 2 10.7576 2.87868 9.87889C3.64706 9.11051 4.82497 9.01406 7 9.00195"
+                      stroke="#4B5563"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M12 2L12 15M12 15L9 11.5M12 15L15 11.5"
+                      stroke="#4B5563"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
                 </div>
-              </label>
-              <input
-                id="file-input"
-                type="file"
-                accept="image/*"
-                onChange={handleImageUpload}
-                hidden
-              />
+              </button>
             </div>
-            <button
-              type="button"
-              className="rounded-md cursor-pointer"
-              onClick={handleDownload}
-            >
-              <div className="flex flex-row justify-center items-center p-3">
+            {actionObjects.length > 0 && (
+              <button
+                type="button"
+                className="flex items-center gap-x-1 px-2 font-semibold text-gray-600"
+                onClick={undo}
+                disabled={actionObjects.length === 0}
+              >
                 <svg
                   viewBox="0 0 24 24"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="w-7 h-7"
+                  className="w-5 h-5"
                 >
                   <path
-                    d="M17 9.00195C19.175 9.01406 20.3529 9.11051 21.1213 9.8789C22 10.7576 22 12.1718 22 15.0002V16.0002C22 18.8286 22 20.2429 21.1213 21.1215C20.2426 22.0002 18.8284 22.0002 16 22.0002H8C5.17157 22.0002 3.75736 22.0002 2.87868 21.1215C2 20.2429 2 18.8286 2 16.0002L2 15.0002C2 12.1718 2 10.7576 2.87868 9.87889C3.64706 9.11051 4.82497 9.01406 7 9.00195"
-                    stroke="#4B5563"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                  />
-                  <path
-                    d="M12 2L12 15M12 15L9 11.5M12 15L15 11.5"
-                    stroke="#4B5563"
+                    d="M4 7H15C16.8692 7 17.8039 7 18.5 7.40193C18.9561 7.66523 19.3348 8.04394 19.5981 8.49999C20 9.19615 20 10.1308 20 12C20 13.8692 20 14.8038 19.5981 15.5C19.3348 15.9561 18.9561 16.3348 18.5 16.5981C17.8039 17 16.8692 17 15 17H8.00001M4 7L7 4M4 7L7 10"
+                    stroke="#1C274C"
                     strokeWidth="1.5"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                   />
                 </svg>
-              </div>
-            </button>
+                <p className="pb-1">Undo</p>
+              </button>
+            )}
           </div>
           <p className="text-xl font-bold mt-10 px-1 mb-3">Toolbar</p>
           <div className="ring-1 ring-gray-200 px-4 py-3 rounded-md">
@@ -281,67 +301,23 @@ const ImageEditor = () => {
           </div>
         </div>
         {/* Canvas */}
+
         <div
-          className="pb-10"
           style={{
-            flex: 2,
+            overflow: "auto",
+            background: "#D3D3D3",
             position: "relative",
             height: "80vh",
+            flex: 2,
           }}
         >
-          <div className="flex justify-center gap-x-2 mb-1">
-            <button
-              type="button"
-              className="border border-gray-400 rounded-md"
-              onClick={undo}
-            >
-              <svg
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-7 h-7"
-              >
-                <path
-                  d="M4 7H15C16.8692 7 17.8039 7 18.5 7.40193C18.9561 7.66523 19.3348 8.04394 19.5981 8.49999C20 9.19615 20 10.1308 20 12C20 13.8692 20 14.8038 19.5981 15.5C19.3348 15.9561 18.9561 16.3348 18.5 16.5981C17.8039 17 16.8692 17 15 17H8.00001M4 7L7 4M4 7L7 10"
-                  stroke="#1C274C"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </button>
-            <button type="button" className="border border-gray-400 rounded-md">
-              <svg
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-7 h-7"
-              >
-                <path
-                  d="M20 7H9.00001C7.13077 7 6.19615 7 5.5 7.40193C5.04395 7.66523 4.66524 8.04394 4.40193 8.49999C4 9.19615 4 10.1308 4 12C4 13.8692 4 14.8038 4.40192 15.5C4.66523 15.9561 5.04394 16.3348 5.5 16.5981C6.19615 17 7.13077 17 9 17H16M20 7L17 4M20 7L17 10"
-                  stroke="#1C274C"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </button>
-          </div>
-          <div
-            style={{
-              overflow: "auto",
-              background: "#D3D3D3",
-              height: "70vh",
-            }}
-          >
-            <canvas
-              ref={canvasRef}
-              onMouseDown={handleTextDragStart}
-              onMouseMove={handleTextDrag}
-              onMouseUp={handleTextDragEnd}
-              onMouseLeave={handleTextDragEnd}
-            />
-          </div>
+          <canvas
+            ref={canvasRef}
+            onMouseDown={handleTextDragStart}
+            onMouseMove={handleTextDrag}
+            onMouseUp={handleTextDragEnd}
+            onMouseLeave={handleTextDragEnd}
+          />
         </div>
       </div>
     </div>
